466ec46bfd50e3d9939e9a8b3d8f78b90b332263
Jakub Łanecki <jakub.lanecki@relia-sol.pl>
2020-05-22 11:58:02 +0200

    GEN & KILL sets simplified.

--- tc/tc/optimization/redundancy.py
+++ tc/tc/optimization/redundancy.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from contextlib import contextmanager
 from tc.common import BaseVisitor
 from tc.globals import global_env
 from tc.parser import Variable
@@ -11,6 +13,7 @@ class EffectiveNodeSearch(BaseVisitor):
     These include:
         - top level 'print' statements
         - top level calls of functions with side effects
+        - TODO: return statements of functions called in effective nodes
     """
 
     def __init__(self):
@@ -114,242 +117,227 @@ class EffectiveNodeSearch(BaseVisitor):
         if self.is_effective(node.name):
             self.effective_nodes.add(node)
 
-    def visit_variable(self, node):
+    def visit_unknown(self, m_name):
         pass
 
-    def visit_literal(self, node):
-        pass
+
+TOP = 'PROGRAM'
+
+
+class VarDefLocator(BaseVisitor):
+    """Finds all variable declarations/assignments in the program."""
+
+    def __init__(self):
+        self.defs = set()
+
+    def reset(self):
+        self.defs = set()
+
+    def run(self, statements):
+        for stmt in statements:
+            self.visit(stmt)
+        return self.defs
+
+    def visit_block(self, node):
+        for stmt in node.statements:
+            self.visit(stmt)
+
+    def visit_function_def(self, node):
+        for p in node.parameters:
+            self.visit(p)
+        self.visit(node.body)
+
+    def visit_variable_declaration(self, node):
+        self.defs.add(node)
+
+    def visit_assignment(self, node):
+        self.defs.add(node)
+
+    def visit_if_stmt(self, node):
+        self.visit(node.body)
+
+    def visit_while_stmt(self, node):
+        self.visit(node.body)
+
+    def visit_for_stmt(self, node):
+        self.visit(node.initializer)
+        self.visit(node.increment)
+        self.visit(node.body)
 
     def visit_unknown(self, m_name):
         pass
 
 
-class Node:
-    """Node of data dependency graph where edges correspond to Use-Define relations."""
-    def __init__(self, node):
-        self.ast_node = node  # corresponding AST node
-        self.deps = set()  # defs needed for given use (Use-Define relationship)
-        self.kills = set()  # set of (name, redefinition node that kills previous definition of name)
+class GenKillBuilder(BaseVisitor):
+    """Statically determines GEN and KILL sets of variable definition nodes for each node of the AST.
 
+    Importantly, we use very crude approximation of GEN and KILL sets for scoped blocks. For GEN sets, we
+    simply take GEN of the inner block statements (so we disregard fact that GENs like inner variable
+    declarations can't be visible outside), which is a blown-up upper bound. For KILL, we take an empty
+    set as a lower bound, so we disregard fact that assignments to outer scope variables are legitimate
+    KILLs.
 
-class DefinitionReach(BaseVisitor):
-    """Statically builds a use-def graph."""
+    TODO: use stack of scopes to approximate them better
+
+    TODO: 'return' statement should only appear at the end of a block?
+
+    Attributes:
+        var_defs (dict): map (name -> set of Assignment/VariableDeclaration nodes) - all variable
+            definitions in the whole program
+        gen (dict): map (AST node -> set of Assignment/VariableDeclaration nodes) - all definitions of
+            variables (assignments or declarations) WITHIN given node that reach the endpoint of this
+            node
+        kill (dict): map (AST node -> set of Assignment/VariableDeclaration nodes) - all definitions
+            of variables within/outside given node that do not reach the endpoint of this node due to
+            reassignment or redeclaration
+    """
 
     def __init__(self):
-        self.scopes = [{
-            'variable': {},
-            'function': {f: Node(None) for f in global_functions}
-        }]
+        self.var_defs = defaultdict(set)
+        self.scopes = [{}]
+        self.gen = {}
+        self.kill = {}
 
     def reset(self):
-        self.scopes = [{
-            'variable': {},
-            'function': {f: Node(None) for f in global_functions}
-        }]
+        self.var_defs = defaultdict(set)
+        self.scopes = [{}]
+        self.gen = {}
+        self.kill = {}
 
-    def push_scope(self):
-        self.scopes.append({'variable': {}, 'function': {}})
+    @contextmanager
+    def in_scope(self):
+        try:
+            self.scopes.append({})
+            yield
+        finally:
+            self.scopes.pop()
 
-    def define(self, name, node, what):
-        self.scopes[-1][what][name] = node
-
-    def resolve(self, name, what):
-        for i in range(len(self.scopes)):
-            if name in self.scopes[-(i + 1)][what]:
-                return i
-        raise Exception(f'Failed to resolve {what} {name}')
-
-    def pop_scope(self):
-        self.scopes.pop()
-
-    def kill(self, kills):
-        for name, node in kills:
-            self.scopes[-1]['variable'][name] = node
+    def resolve(self, name):
+        for scope in enumerate(reversed(self.scopes)):
+            if name in scope:
+                return scope[name]
+        raise Exception(f'Failed to resolve function {name}')
 
     def run(self, statements):
-        # Build the dependency graph
-        ext_statements = []
-        for stmt in statements:
-            ext_statements.append(self.visit(stmt))
+        self.gather_defs(statements)
+        return self.visit_statements(statements)
+
+    def gather_defs(self, statements):
+        locator = VarDefLocator()
+        def_nodes = locator.run(statements)
+        for node in def_nodes:
+            self.var_defs[node.name].add(node)
+
+    def visit_statements(self, statements):
+        gen, kill = set(), set()
+
+        self.visit(statements[0])
+        gen |= self.gen[statements[0]]
+        kill |= self.kill[statements[0]]
+
+        for i, stmt in enumerate(statements[1:]):
+            self.visit(stmt)
+            gen -= self.kill[stmt]
+            gen |= self.gen[stmt]
+
+            kill -= self.gen[stmt]  # NOTE: is it even used?
+            kill |= self.kill[stmt]
+
+        return gen, kill
 
     def visit_block(self, node):
-        b_node = Node(node)
-
-        self.push_scope()
-        try:
-            for stmt in node.statements:
-                n = self.visit(stmt)
-                n.deps.add(b_node)
-                b_node.kills.update(n.kills)
-        except RedundancyOptimizer.Return as r:
-            b_node.kills.update(r.node.kills)
-            b_node.deps.add(r.node)
-        self.pop_scope()
-
-        return b_node
+        with self.in_scope():
+            gen, _ = self.visit_statements(node.statements)
+            self.gen[node] = gen
+            self.kill[node] = set()
 
     def visit_function_def(self, node):
-        f_node = Node(node)
+        self.visit(node.body)
+        self.gen[node] = set()
+        self.kill[node] = set()
 
-        self.push_scope()
-        for p in node.parameters:
-            n = Node(node)
-            self.define(p.name, n, 'variable')
+        p_names = {param.name for param in node.parameters}
+        gen = {node for node in self.gen[node.body] if node.name not in p_names}
+        kill = {node for node in self.kill[node.body] if node.name not in p_names}
 
-        b_node = self.visit(node.body)
-        self.pop_scope()
-
-        f_node.deps = b_node.deps
-        f_node.kills = b_node.kills
-        self.define(node.name, f_node, 'function')
-
-        return f_node
-
-    def visit_print_stmt(self, node):
-        p_node = Node(node)
-        n = self.visit(node.expr)
-
-        p_node.deps.add(n)
-        self.kill(n.kills)
-
-        self.effective_nodes.add(p_node)
-        return p_node
+        self.scopes[-1][node.name] = (gen, kill)  # GEN and KILL for function calls
 
     def visit_variable_declaration(self, node):
-        v_node = Node(node)
-
         if node.value:
-            n = self.visit(node.value)
-            v_node.deps.add(n)
-            v_node.kills.update(n.kills)
-
-        self.define(node.name, v_node, 'variable')
-        return v_node
+            self.visit_assignment(node)
+        else:
+            self.gen[node] = {node}
+            self.kill[node] = self.var_defs[node.name] - {node}
 
     def visit_assignment(self, node):
-        a_node = Node(node)
-        n = self.visit(node.value)
+        self.visit(node.value)
+        self.gen[node] = {node} | (self.gen[node.value] - self.var_defs[node.name])
+        self.kill[node] = (self.var_defs[node.name] | self.kill[node.value]) - {node}
 
-        a_node.deps.add(n)
-        a_node.kills.add((node.name, a_node))
+    def carry(self, node, source_node):
+        self.visit(source_node)
+        self.gen[node] = self.gen[source_node]
+        self.kill[node] = self.kill[source_node]
 
-        depth = self.resolve(node.name, 'variable')
-        self.scopes[-(depth + 1)]['variable'][node.name] = a_node
-
-        return a_node
+    def visit_print_stmt(self, node):
+        self.carry(node, node.expr)
 
     def visit_if_stmt(self, node):
-        i_node = Node(node)
-        c_node = self.visit(node.condition)
-        i_node.deps.add(c_node)
+        stmt_list = [node.condition, node.body]
+        gen, kill = self.visit_statements(stmt_list)
 
-        b_node = self.visit(node.body)
-        b_node.deps.add(i_node)
-        i_node.kills.update(b_node.kills)
-
-        return i_node
+        self.gen[node] = gen
+        self.kill[node] = kill
 
     def visit_while_stmt(self, node):
-        w_node = Node(node)
-        c_node = self.visit(node.condition)
-        w_node.deps.add(c_node)
+        self.visit(node.condition)
+        self.visit(node.body)
 
-        from copy import deepcopy
-        scopy = [deepcopy(s) for s in self.scopes]
-
-        b_node = self.visit(node.body)
-        w_node.kills.update(b_node.kills)
-        b_node.deps.add(w_node)
-        import pprint as pp
-        pp.pprint(b_node.kills)
-
-        # Variables in loop condition depend on redefinitions in loop body!
-        # TODO: separation of data dependencies from AST structure would do better?
-        cond_deps = set()
-        for n in c_node.deps:
-            if isinstance(n.ast_node, Variable):
-                cond_deps.add(n.ast_node.name)
-
-        for name, node in w_node.kills:
-            if name in cond_deps:
-                c_node.deps.add(node)
-
-            # TODO:???
-            for sc in reversed(scopy):
-                if name in sc['variable']:
-                    sc['variable'][name].deps.add(node)
-
-        return w_node
+        self.gen[node] = self.gen[node.condition] | self.gen[node.body]
+        self.kill[node] = self.kill[node.condition] & self.kill[node.body]
 
     def visit_for_stmt(self, node):
-        f_node = Node(node)
-        self.push_scope()
+        self.visit(node.initializer)
+        self.visit(node.condition)
+        self.visit(node.increment)
+        self.visit(node.body)
 
-        init_node = self.visit(node.initializer)
-        c_node = self.visit(node.condition)
-        inc_node = self.visit(node.increment)
+        gen = self.gen[node.initializer] - self.kill[node.condition]
+        gen |= self.gen[node.body] | self.gen[node.increment]
 
-        b_node = self.visit(node.body)
-        b_node.deps.add(f_node)
-        f_node.deps.update({
-            init_node, c_node, inc_node
-        })
+        kill = self.kill[node.initializer] - (
+            self.gen[node.condition] | self.gen[node.body] | self.gen[node.increment]
+        )
 
-        self.pop_scope()
-        return f_node
+        self.gen[node] = gen
+        self.kill[node] = kill
 
     def visit_binary_expr(self, node):
-        b_node = Node(node)
-        l_node = self.visit(node.left)
-        r_node = self.visit(node.right)
+        stmt_list = [node.left, node.right]
+        gen, kill = self.visit_statements(stmt_list)
 
-        b_node.deps.update({l_node, r_node})
-        b_node.kills.update(l_node.kills | r_node.kills)
-        return b_node
+        self.gen[node] = gen
+        self.kill[node] = kill
 
     def visit_unary_expr(self, node):
-        u_node = Node(node)
-        e_node = self.visit(node.expr)
-
-        u_node.deps.add(e_node)
-        u_node.kills.update(e_node.kills)
-        return u_node
-
-    class Return(Exception):
-        def __init__(self, return_node):
-            super()
-            self.node = return_node
+        self.carry(node, node.expr)
 
     def visit_return_stmt(self, node):
-        r_node = Node(node)
-        e_node = self.visit(node.expr)
-        r_node.deps.add(e_node)
-        r_node.kills.update(e_node.kills)
-        raise RedundancyOptimizer.Return(r_node)
+        self.carry(node, node.expr)
 
     def visit_call(self, node):
-        c_node = Node(node)
-
-        for a in node.args:
-            n = self.visit(a)
-            c_node.deps.add(n)
-
-        f_depth = self.resolve(node.name, 'function')
-        f_node = self.scopes[-(f_depth + 1)]['function'][node.name]
-        c_node.deps.add(f_node)
-        self.kill(f_node.kills)
-
-        return c_node
+        gen, kill = self.visit_statements(node.args)
+        f_gen, f_kill = self.resolve(node.name)
+        self.gen[node] = f_gen | (gen - f_kill)
+        self.kill[node] = f_kill | (kill - f_gen)
 
     def visit_variable(self, node):
-        v_node = Node(node)
-        v_depth = self.resolve(node.name, 'variable')
-        value_node = self.scopes[-(v_depth + 1)]['variable'][node.name]
-        v_node.deps.add(value_node)
-        return v_node
+        self.gen[node] = set()
+        self.kill[node] = set()
 
     def visit_literal(self, node):
-        return Node(node)
+        self.gen[node] = set()
+        self.kill[node] = set()
 
     def visit_unknown(self, m_name):
         pass
@@ -360,13 +348,19 @@ class RedundancyOptimizer(BaseVisitor):
 
     def __init__(self):
         self.effective_search = EffectiveNodeSearch()
-        self.def_reach = DefinitionReach()
+        self.use_def = GenKillBuilder()
 
     def reset(self):
         self.effective_search.reset()
-        self.def_reach.reset()
+        self.use_def.reset()
 
     def run(self, statements):
+        # TODO:
+        #  - find effective nodes
+        #  - build use-def table
+        #  - traverse AST from effective nodes via use-def table to find all required nodes
+        #  - prune all nodes that were not found required
+
         # Build the dependency graph
         ext_statements = []
         for stmt in statements:
